a7c5e3034406aaf32db9965717bdf1d680eafdeb
Sv <ranganathan.sv@accenture.com>
2023-02-02 13:18:47 +0530

    added hosted backend url to axios requests

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import {legacy_createStore as createStore} from "redux"
 import reducer from "./reducer"
-const PORT = require("../api/server_port")
+import { endpoint } from "../../src/components/ENDPOINT"
 
-const base_url = "http://localhost:" + PORT + "/pizzeria"
+const base_url = endpoint + "/pizzeria"
 const store = createStore(reducer, {base_url: base_url})
 
 export default store
